15410c6fdaa02f4232f228f7a4b896b8065f07b9
nicolas <cnico_97@hotmail.com>
2021-08-19 13:59:52 -0300

    routing on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {useDispatch} from 'react-redux';
+import {Route} from 'react-router-dom';
+import {All} from './components/all/All';
+import {Users} from './components/users/Users';
+import {setAccounts, setTransactions, setUsers} from './redux/actions';
 
-function App() {
+async function App() {
+    const dispatch = useDispatch();
+    const usersRes = await axios.get('http://localhost:3000/api2/transactions');
+    if (usersRes.data) dispatch(setUsers(usersRes.data));
+    const accountsRes = await axios.get('http://localhost:3001/api/contacts/');
+    if (accountsRes.data) dispatch(setAccounts(accountsRes.data));
+    const transactionsRes = await axios.get('http://localhost:3000/api2/account');
+    if (transactionsRes.data) dispatch(setTransactions(transactionsRes.data));
     return (
-        <div className="App">
-            <h1>admin app</h1>
+        <div>
+            <Route exact path="/">
+                <All />
+            </Route>
+            <Route path="/users">
+                <Users />
+            </Route>
         </div>
     );
 }
