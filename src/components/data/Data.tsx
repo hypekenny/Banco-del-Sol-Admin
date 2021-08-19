c5cbd082b17ae9bf3361b27a254b6392bc0b00e0
nicolas <cnico_97@hotmail.com>
2021-08-19 20:09:31 -0300

    creating component data

---
+++ src/components/data/Data.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './UsersStyles.module.css';
+
+export function Data() {
+    return (
+        <div>
+            <div>
+                <label>USERS</label>
+            </div>
+        </div>
+    );
+}
