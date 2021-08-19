cf4ae089750264cac295adde0d7db3b347d44087
nicolas <cnico_97@hotmail.com>
2021-08-19 20:08:20 -0300

    creating component account

---
+++ src/components/accounts/Accounts.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {rootState} from '../../types';
+import styles from './AccountsStyles.module.css';
+import constants from '../constants.module.css';
+
+export function Accounts() {
+    const allAccounts = useSelector((state: rootState) => state.account);
+    return (
+        <div className={styles.container}>
+            <div>
+                {allAccounts &&
+                    allAccounts.map((acc, i) => (
+                        <div key={i} className={constants.card}>
+                            <p>email: {acc.email}</p>
+                            <p>cvu: {acc.cvu}</p>
+                            <p>amount: {acc.balance && acc.balance.amount}</p>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
+}
