d3589ad708280585cbb5653e90bdc2fce5b2efdb
nicolas <cnico_97@hotmail.com>
2021-08-20 10:36:47 -0300

    add

--- src/components/data/Data.tsx
+++ src/components/data/Data.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
-// import styles from './UsersStyles.module.css';
+import {Line, LineChart, ResponsiveContainer, XAxis} from 'recharts';
+import styles from './DataStyles.module.css';
 
 export function Data() {
+    const data = [
+        {
+            month: 'Jan',
+            amount: 200,
+        },
+        {
+            month: 'Feb',
+            amount: 123,
+        },
+        {
+            month: 'Mar',
+            amount: 515,
+        },
+        {
+            month: 'Apr',
+            amount: 7357,
+        },
+        {
+            month: 'may',
+            amount: 579,
+        },
+        {
+            month: 'Jun',
+            amount: 246,
+        },
+        {
+            month: 'Jul',
+            amount: 345,
+        },
+        {
+            month: 'Agu',
+            amount: 6246,
+        },
+        {
+            month: 'Sep',
+            amount: 2624,
+        },
+        {
+            month: 'Oct',
+            amount: 650,
+        },
+        {
+            month: 'Nov',
+            amount: 2020,
+        },
+        {
+            month: 'Dec',
+            amount: 2010,
+        },
+    ];
     return (
-        <div>
-            <div>
-                <label>USERS</label>
+        <div className={Styles.chart}>
+            <div className={Styles.chart}>
+                <ResponsiveContainer width="100%" aspect={4 / 1}>
+                    <LineChart data={data}>
+                        <XAxis dataKey="month" stroke="#ff4b6e" />
+                        <Line type="monotone" dataKey="amount" stroke="#ff4b6e" />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,12 +4,17 @@ import {Users} from '../users/Users';
 import {Accounts} from '../accounts/Accounts';
 import {Transactions} from '../transactions/Transactions';
 import {useState} from 'react';
+import {Data} from '../data/Data';
 
 export function All() {
-    const [view, setView] = useState('all');
+    const [view, setView] = useState('data');
     return (
         <div className={styles.container}>
             <div className={styles.sidebar}>
+                <h1 className={styles.title}>Banco del Sol Admin App</h1>
+                <button className={styles.btn} onClick={() => setView('data')}>
+                    data
+                </button>
                 <button className={styles.btn} onClick={() => setView('all')}>
                     all
                 </button>
@@ -22,11 +27,9 @@ export function All() {
                 <button className={styles.btn} onClick={() => setView('tran')}>
                     transactions
                 </button>
-                <button className={styles.btn} onClick={() => setView('data')}>
-                    data
-                </button>
             </div>
             <div className={styles.information}>
+                {view === 'data' ? <Data /> : null}
                 {view === 'all' || view === 'user' ? <Users /> : null}
                 {view === 'all' || view === 'acc' ? <Accounts /> : null}
                 {view === 'all' || view === 'tran' ? <Transactions /> : null}
