04a6ee0fce7da815b85827aedc6c36595981652f
nicolas <cnico_97@hotmail.com>
2021-08-21 16:59:52 -0300

    making better user component

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -3,20 +3,47 @@ import {useSelector} from 'react-redux';
 import {rootState} from '../constants/types';
 import styles from './UsersStyles.module.css';
 import constants from '../constants/constants.module.css';
+import {useState} from 'react';
 
 export function Users() {
     const allUsers = useSelector((state: rootState) => state.user);
+    const [view, setView] = useState('all');
     return (
         <div className={styles.container}>
-            <div>
-                {allUsers &&
-                    allUsers.map((user, i) => (
-                        <div key={i} className={constants.card}>
-                            <p>name: {user.name}</p>
-                            <p>lastname: {user.lastName}</p>
-                        </div>
-                    ))}
+            <div className={styles.buttons}>
+                <div>
+                    <button className={constants.btn} onClick={() => setView('create')}>
+                        all
+                    </button>
+                </div>
+                <div>
+                    <button className={constants.btn} onClick={() => setView('create')}>
+                        create
+                    </button>
+                </div>
+                <div>
+                    <button className={constants.btn} onClick={() => setView('create')}>
+                        deleted users
+                    </button>
+                </div>
             </div>
+
+            {view === 'all' ? (
+                <div>
+                    {allUsers &&
+                        allUsers.map((user, i) => (
+                            <div key={i} className={constants.card}>
+                                <input type="checkbox"></input>
+                                <label className={constants.text}>email: {user.email}</label>
+                                <label className={constants.text}>name: {user.name}</label>
+                                <label className={constants.text}>lastname: {user.lastName}</label>
+                                <label className={constants.text}>dni: {user.dni}</label>
+                                <label className={constants.text}>phonenumber: {user.phoneNumber}</label>
+                            </div>
+                        ))}
+                </div>
+            ) : null}
+            {view === 'create' ? <div>pending</div> : null}
         </div>
     );
 }
