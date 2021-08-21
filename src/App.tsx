a8990137364eda344582bd1d01c9ce51053254f8
nicolas <cnico_97@hotmail.com>
2021-08-21 16:24:10 -0300

    fixing the firs data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -10,41 +10,33 @@ import {setAccounts, setTransactions, setUsers} from './redux/actions';
 import firebase from 'firebase';
 import {firebaseConfig} from './components/constants/firebase.config';
 require('firebase/firebase-auth');
-
 firebase.initializeApp(firebaseConfig);
 
 export function App() {
     const dispatch = useDispatch();
 
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword('admin@admin.com', 'admin')
-        .then((response) => {
-            response.user?.getIdToken().then((idToken) => {
-                console.log(idToken);
+    (async function () {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword('admin@admin.com', 'admin123')
+            .then((response) => {
+                response.user?.getIdToken().then(async (idToken) => {
+                    const usersRes = await axios.get('http://localhost:3001/api/user/all', {
+                        headers: {
+                            authorization: `Bearer ${idToken}`,
+                        },
+                    });
+                    if (usersRes.data) dispatch(setUsers(usersRes.data));
+
+                    const accountsRes = await axios.get('http://localhost:3000/api2/account');
+                    if (accountsRes.data) dispatch(setAccounts(accountsRes.data));
+
+                    const transactionsRes = await axios.get('http://localhost:3000/api2/transactions');
+                    if (transactionsRes.data) dispatch(setTransactions(transactionsRes.data));
+                });
             });
-        });
-    // axios
-    //   .get<resFromBack>(
-    //     `http://192.168.0.4:3001/api/user/?email=${email}`,
-    //     {
-    //       headers: {
-    //         authorization: `Bearer ${idToken}`,
-    //       },
-    //     },
-    //   )
+    })();
 
-    async function get() {
-        const usersRes = await axios.get('http://localhost:3001/api/contacts');
-        if (usersRes.data) dispatch(setUsers(usersRes.data));
-
-        const accountsRes = await axios.get('http://localhost:3000/api2/account');
-        if (accountsRes.data) dispatch(setAccounts(accountsRes.data));
-
-        const transactionsRes = await axios.get('http://localhost:3000/api2/transactions');
-        if (transactionsRes.data) dispatch(setTransactions(transactionsRes.data));
-    }
-    get();
     return (
         <div>
             <Route exact path="/">
