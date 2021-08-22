0cdcb7268071d6ac95907197402c79fb4edf9f52
nicolas <cnico_97@hotmail.com>
2021-08-22 02:26:43 -0300

    using the functions for geting and setings the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import {useDispatch} from 'react-redux';
 import {Route} from 'react-router-dom';
 import {Accounts} from './components/accounts/Accounts';
@@ -6,9 +5,9 @@ import {All} from './components/home/Home';
 import {Data} from './components/data/Data';
 import {Transactions} from './components/transactions/Transactions';
 import {Users} from './components/users/Users';
-import {setAccounts, setTransactions, setUsers} from './redux/actions';
-import firebase from 'firebase';
+import {getTransactions, getUsers, getAccounts, setToken} from './redux/actions';
 import {firebaseConfig} from './components/constants/firebase.config';
+import firebase from 'firebase';
 require('firebase/firebase-auth');
 firebase.initializeApp(firebaseConfig);
 
@@ -21,19 +20,16 @@ export function App() {
             .signInWithEmailAndPassword('admin@admin.com', 'admin123')
             .then((response) => {
                 response.user?.getIdToken().then(async (idToken) => {
-                    const usersRes = await axios.get('http://localhost:3001/api/user/all', {
-                        headers: {
-                            authorization: `Bearer ${idToken}`,
-                        },
-                    });
-                    if (usersRes.data) dispatch(setUsers(usersRes.data));
+                    dispatch(setToken(idToken));
 
-                    const accountsRes = await axios.get('http://localhost:3000/api2/account');
-                    if (accountsRes.data) dispatch(setAccounts(accountsRes.data));
+                    dispatch(getUsers(idToken));
 
-                    dispatch(setTransactions());
+                    dispatch(getAccounts());
+
+                    dispatch(getTransactions());
                 });
-            });
+            })
+            .catch((error) => console.log(error));
     })();
 
     return (
