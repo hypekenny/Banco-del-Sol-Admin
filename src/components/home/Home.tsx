3d589367c851d3325a190f4f8e406a42f884de38
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2021-08-22 22:04:31 -0300

    menu finalizado

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,37 +1,48 @@
-import React from 'react';
-import styles from './HomeStyles.module.css';
-import {Users} from '../users/Users';
-import {Accounts} from '../accounts/Accounts';
-import {Transactions} from '../transactions/Transactions';
-import {useState} from 'react';
-import {Data} from '../data/Data';
-import constants from '../constants/constants.module.css';
+import React from "react";
+import styles from "./HomeStyles.module.css";
+import { Users } from "../users/Users";
+import { Accounts } from "../accounts/Accounts";
+import { Transactions } from "../transactions/Transactions";
+import { useState } from "react";
+import { Data } from "../data/Data";
 
 export function All() {
-    const [view, setView] = useState('data');
-    return (
-        <div className={styles.container}>
-            <div className={styles.sidebar}>
-                <h1 className={styles.title}>Banco del Sol Admin App</h1>
-                <button className={constants.btn} onClick={() => setView('data')}>
-                    data
-                </button>
-                <button className={constants.btn} onClick={() => setView('user')}>
-                    users
-                </button>
-                <button className={constants.btn} onClick={() => setView('acc')}>
-                    accounts
-                </button>
-                <button className={constants.btn} onClick={() => setView('tran')}>
-                    transactions
-                </button>
-            </div>
-            <div className={styles.information}>
-                {view === 'data' ? <Data /> : null}
-                {view === 'user' ? <Users /> : null}
-                {view === 'acc' ? <Accounts /> : null}
-                {view === 'tran' ? <Transactions /> : null}
-            </div>
+  const [view, setView] = useState("data");
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        {/* Sacar clase del div 17 */}
+        <div className={styles.divImgLogo}>
+          <img
+            className={styles.imgLogo}
+            src="https://cdn.discordapp.com/attachments/872492726397042688/874643755158892594/Banco-del-Sol-Logo.png"
+            alt=""
+          />
         </div>
-    );
+        <div className={styles.elipse}></div>
+
+        <button className={styles.btn} onClick={() => setView("data")}>
+          DATA
+        </button>
+
+        <button className={styles.btn} onClick={() => setView("user")}>
+          USER
+        </button>
+
+        <button className={styles.btn} onClick={() => setView("acc")}>
+          ACCOUNT
+        </button>
+
+        <button className={styles.btn} onClick={() => setView("tran")}>
+          TRANSACCIONES
+        </button>
+      </div>
+      <div className={styles.information}>
+        {view === "data" ? <Data /> : null}
+        {view === "user" ? <Users /> : null}
+        {view === "acc" ? <Accounts /> : null}
+        {view === "tran" ? <Transactions /> : null}
+      </div>
+    </div>
+  );
 }
