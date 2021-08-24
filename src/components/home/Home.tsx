9b2d7ebfe862e3ff6f807d0b6f45328ea8b6a76a
nicolas <cnico_97@hotmail.com>
2021-08-24 14:22:30 -0300

    adding welcome screen

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,48 +1,49 @@
-import React from "react";
-import styles from "./HomeStyles.module.css";
-import { Users } from "../users/Users";
-import { Accounts } from "../accounts/Accounts";
-import { Transactions } from "../transactions/Transactions";
-import { useState } from "react";
-import { Data } from "../data/Data";
+import React from 'react';
+import styles from './HomeStyles.module.css';
+import {Users} from '../users/Users';
+import {Accounts} from '../accounts/Accounts';
+import {Transactions} from '../transactions/Transactions';
+import {useState} from 'react';
+import {Data} from '../data/Data';
+import {Welcome} from '../welcome/Welcome';
 
 export function All() {
-  const [view, setView] = useState("data");
-  return (
-    <div className={styles.container}>
-      <div className={styles.sidebar}>
-        {/* Sacar clase del div 17 */}
-        <div className={styles.divImgLogo}>
-          <img
-            className={styles.imgLogo}
-            src="https://cdn.discordapp.com/attachments/872492726397042688/874643755158892594/Banco-del-Sol-Logo.png"
-            alt=""
-          />
+    const [view, setView] = useState('welcome');
+    return (
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <div className={styles.divImgLogo} onClick={() => setView('welcome')}>
+                    <img
+                        className={styles.imgLogo}
+                        src="https://cdn.discordapp.com/attachments/872492726397042688/874643755158892594/Banco-del-Sol-Logo.png"
+                        alt=""
+                    />
+                </div>
+                <div className={styles.elipse}></div>
+
+                <button className={styles.btn} onClick={() => setView('data')}>
+                    DATA
+                </button>
+
+                <button className={styles.btn} onClick={() => setView('user')}>
+                    USUARIO
+                </button>
+
+                <button className={styles.btn} onClick={() => setView('acc')}>
+                    CUENTA
+                </button>
+
+                <button className={styles.btn} onClick={() => setView('tran')}>
+                    TRANSACCIONES
+                </button>
+            </div>
+            <div className={styles.information}>
+                {view === 'welcome' ? <Welcome /> : null}
+                {view === 'data' ? <Data /> : null}
+                {view === 'user' ? <Users /> : null}
+                {view === 'acc' ? <Accounts /> : null}
+                {view === 'tran' ? <Transactions /> : null}
+            </div>
         </div>
-        <div className={styles.elipse}></div>
-
-        <button className={styles.btn} onClick={() => setView("data")}>
-          DATA
-        </button>
-
-        <button className={styles.btn} onClick={() => setView("user")}>
-          USUARIO
-        </button>
-
-        <button className={styles.btn} onClick={() => setView("acc")}>
-          CUENTA
-        </button>
-
-        <button className={styles.btn} onClick={() => setView("tran")}>
-          TRANSACCIONES
-        </button>
-      </div>
-      <div className={styles.information}>
-        {view === "data" ? <Data /> : null}
-        {view === "user" ? <Users /> : null}
-        {view === "acc" ? <Accounts /> : null}
-        {view === "tran" ? <Transactions /> : null}
-      </div>
-    </div>
-  );
+    );
 }
