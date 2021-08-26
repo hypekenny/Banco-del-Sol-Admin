ae09f7974c4040b5d772a1c3c37b7e4caec4d09a
nicolas <cnico_97@hotmail.com>
2021-08-26 12:58:32 -0300

    removing auto login and seters for the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,12 @@ import {All} from './components/home/Home';
 import {Data} from './components/data/Data';
 import {Transactions} from './components/transactions/Transactions';
 import {Users} from './components/users/Users';
-import {getTransactions, getUsers, getAccounts, setToken} from './redux/actions';
 import {firebaseConfig} from './components/constants/firebase.config';
 import firebase from 'firebase';
 require('firebase/firebase-auth');
 firebase.initializeApp(firebaseConfig);
 
 export function App() {
-    const dispatch = useDispatch();
-
-    (async function () {
-        firebase
-            .auth()
-            .signInWithEmailAndPassword('admin@admin.com', 'admin123')
-            .then((response) => {
-                response.user?.getIdToken().then(async (idToken) => {
-                    dispatch(setToken(idToken));
-
-                    dispatch(getUsers(idToken));
-
-                    dispatch(getAccounts());
-
-                    dispatch(getTransactions());
-                });
-            })
-            .catch((error) => console.log(error));
-    })();
-
     return (
         <div>
             <Route exact path="/">
